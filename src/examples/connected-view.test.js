4ec28daabd10645bfbec708bdc445ae0315e6f78
huitou <3.riverside.swe@gmail.com>
2019-07-28 00:01:51 +0100

    adding license note

--- src/examples/connected-view.test.js
+++ src/examples/connected-view.test.js
@@ -1,3 +1,11 @@
+/*
+    Connected View Test.
+
+    Copyright (c) 2019 Riverside Software Engineering Ltd. All rights reserved.
+
+    Licensed under the MIT License. See LICENSE file in the project root for full license information.
+*/
+
 import React from 'react';
 import { mount } from "enzyme";
 import { mergeDeepRight } from 'ramda';
