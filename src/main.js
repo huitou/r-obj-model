e2050bc213c2f43292b44683db7f9a692e9a7350
huitou <3.riverside.swe@gmail.com>
2019-07-28 18:21:59 +0100

    aligning core lib and prep for export

--- src/main.js
+++ src/main.js
@@ -6,6 +6,6 @@
     Licensed under the MIT License. See LICENSE file in the project root for full license information.
 */
 
-import NewlyDesignedModel from './obj-model/index';
+import CldObjectModel from './obj-model/index';
 
-export { NewlyDesignedModel };
+export { CldObjectModel };
